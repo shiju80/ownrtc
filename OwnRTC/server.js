8e0fe5da65230e71204557a27ae0e5abc48d604c
Shiju <shiju@iamplus.com>
2018-10-29 09:55:28 +0530

    Updated port to 80

--- OwnRTC/server.js
+++ OwnRTC/server.js
@@ -1,8 +1,8 @@
 // Load required modules
-var http    = require("http");              // http server core module
-var express = require("express");           // web framework external module
-var serveStatic = require('serve-static');  // serve static files
-var socketIo = require("socket.io");        // web socket external module
+var http = require("http"); // http server core module
+var express = require("express"); // web framework external module
+var serveStatic = require('serve-static'); // serve static files
+var socketIo = require("socket.io"); // web socket external module
 
 // This sample is using the easyrtc from parent folder.
 // To use this server_example folder only without parent folder:
@@ -16,43 +16,49 @@ process.title = "node-easyrtc";
 
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 var app = express();
-app.use(serveStatic('static', {'index': ['index.html']}));
+app.use(serveStatic('static', {
+    'index': ['index.html']
+}));
 
 // Start Express http server on port 8080
 var webServer = http.createServer(app);
 
 // Start Socket.io so it attaches itself to Express server
-var socketServer = socketIo.listen(webServer, {"log level":1});
+var socketServer = socketIo.listen(webServer, {
+    "log level": 1
+});
 
 easyrtc.setOption("logLevel", "debug");
 
 // Overriding the default easyrtcAuth listener, only so we can directly access its callback
-easyrtc.events.on("easyrtcAuth", function(socket, easyrtcid, msg, socketCallback, callback) {
-    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function(err, connectionObj){
+easyrtc.events.on("easyrtcAuth", function (socket, easyrtcid, msg, socketCallback, callback) {
+    easyrtc.events.defaultListeners.easyrtcAuth(socket, easyrtcid, msg, socketCallback, function (err, connectionObj) {
         if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
             callback(err, connectionObj);
             return;
         }
 
-        connectionObj.setField("credential", msg.msgData.credential, {"isShared":false});
+        connectionObj.setField("credential", msg.msgData.credential, {
+            "isShared": false
+        });
 
-        console.log("["+easyrtcid+"] Credential saved!", connectionObj.getFieldValueSync("credential"));
+        console.log("[" + easyrtcid + "] Credential saved!", connectionObj.getFieldValueSync("credential"));
 
         callback(err, connectionObj);
     });
 });
 
 // To test, lets print the credential to the console for every room join!
-easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, callback) {
-    console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
+easyrtc.events.on("roomJoin", function (connectionObj, roomName, roomParameter, callback) {
+    console.log("[" + connectionObj.getEasyrtcid() + "] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
     easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
 });
 
 // Start EasyRTC server
-var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
+var rtc = easyrtc.listen(app, socketServer, null, function (err, rtcRef) {
     console.log("Initiated");
 
-    rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
+    rtcRef.events.on("roomCreate", function (appObj, creatorConnectionObj, roomName, roomOptions, callback) {
         console.log("roomCreate fired! Trying to create: " + roomName);
 
         appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
@@ -60,6 +66,6 @@ var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
 });
 
 //listen on port 8080
-webServer.listen(8080, function () {
-    console.log('listening on http://localhost:8080');
+webServer.listen(80, function () {
+    console.log('listening on http://localhost:80');
 });
